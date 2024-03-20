aee34db54b5b7aae3e1e7c06159a314d764c0564
arturovallemacias <arturo_valle@live.com>
2024-03-20 21:21:43 +0000

    Actualización del repositorio con nuevos archivos y carpetas

--- src/gen.py
+++ src/gen.py
@@ -15,7 +15,7 @@ else:
     print("La ruta no está en sys.path, añadiéndola...")
     sys.path.append(ruta_especifica)
 
-from core.sdsetup import estaclase
+#from core.sdsetup import estaclase
 from core.args import main_args, samplers
 from core.text import read_txt, multiprompt
 from core.utils import load_img, save_img, calc_size, isok, isset, img_list, basename, progbar, save_cfg
