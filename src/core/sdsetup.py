02a172e74a91d23f9fd84953dd24a7c908be12cf
arturovallemacias <arturo_valle@live.com>
2024-03-22 22:26:22 +0000

    Actualización del repositorio con nuevos archivos y carpetas

--- src/core/sdsetup.py
+++ src/core/sdsetup.py
@@ -1,4 +1,45 @@
+
+import os, sys
+import time
+import numpy as np
+from contextlib import nullcontext
+from einops import rearrange
+
+import torch
+import torch.nn.functional as F
+
+from transformers import CLIPTextModel, CLIPTokenizer
+from diffusers.models import AutoencoderKL
+from diffusers import StableDiffusionPipeline
+from diffusers.utils import is_accelerate_available, is_accelerate_version
+
+
+ruta_especifica = "/content/SDFU/src/xtra"
+if ruta_especifica in sys.path:
+    print("La ruta ya está en sys.path")
+else:
+    print("La ruta no está en sys.path, añadiéndola...")
+    sys.path.append(ruta_especifica)
+
+
+from .text import multiprompt
+from .utils import img_list, load_img, makemask, isok, isset, progbar, file_list
+from .args import models, unprompt
+
+import logging
+
+
+
 class sdfu:
     def __init__(self,a):
         print(a.in_txt)
         print("act")
+
+
+
+
+
+
+
+
+
